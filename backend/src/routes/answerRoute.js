ffea93a9d6d9802e0386e3b460a786262d920c1e
Rafy Tho <rafytho30@gmail.com>
2026-04-22 23:15:43 +0700

    Add update and delete endpoints for answers with role-based authorization
    Import deleteAnswer and updateAnswer controllers, add authorize middleware with INSTRUCTOR and ADMIN role constants. Implement PATCH and DELETE routes on /:id endpoint requiring authentication and instructor/admin authorization.

--- backend/src/routes/answerRoute.js
+++ backend/src/routes/answerRoute.js
@@ -2,12 +2,21 @@ import express from "express";
 import requireAuth from "../middlewares/requireAuth.js";
 import { answerValidator } from "../validators/questionValidators.js";
 import { validateResult } from "../middlewares/validateResult.js";
-import { createAnswer } from "../controllers/answerControllers.js";
+import {
+  createAnswer,
+  deleteAnswer,
+  updateAnswer,
+} from "../controllers/answerControllers.js";
+import authorize from "../middlewares/authorize.js";
+import { ADMIN, INSTRUCTOR } from "../constants/constants.js";
 
 const answerRoute = express.Router({ mergeParams: true });
 
 answerRoute
   .route("/")
   .post(requireAuth, answerValidator, validateResult, createAnswer);
-
+answerRoute
+  .route("/:id")
+  .patch(requireAuth, authorize(INSTRUCTOR, ADMIN), updateAnswer)
+  .delete(requireAuth, authorize(INSTRUCTOR, ADMIN), deleteAnswer);
 export default answerRoute;
